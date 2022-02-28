e64a8cb793b60805f6c7bace6d119d17d2f82ced
devmateusramos <devmateusramos@gmail.com>
2022-02-28 20:48:02 -0300

    looping

--- m04- looping/main.go
+++ m04- looping/main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+	nomes := []string{"Mateus", "Ramos", "Vencendor"}
+	for j := 0; j < len(nomes); j++ {
+		fmt.Println(nomes[j])
+	}
+
+	for k, nome := range nomes {
+		fmt.Println(k, nome)
+	}
+
+	for _, nome := range nomes {
+		fmt.Println(nome)
+	}
+
+	var i int
+
+	for i < len(nomes) {
+		fmt.Println(nomes[i] + " sim")
+		i++
+	}
+	/*
+		Outro uso do for é para um looping infinito como por exemplo abrir um server
+		q vc quer fique escutando sem parar
+		for {
+			fmt.Println(nomes[i])
+			i++
+		}
+	*/
+}
